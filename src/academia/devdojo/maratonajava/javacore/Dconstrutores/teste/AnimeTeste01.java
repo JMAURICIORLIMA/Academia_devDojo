aaf96a30b682bf55ead3d3005897b4c566b49814
José Maurício <mauricio.rodriguesdelima@gmail.com>
2022-05-18 14:59:35 -0300

    Aula 59 - Orientação Objetos - Construtores pt 2;

--- src/academia/devdojo/maratonajava/javacore/Dconstrutores/teste/AnimeTeste01.java
+++ src/academia/devdojo/maratonajava/javacore/Dconstrutores/teste/AnimeTeste01.java
@@ -5,9 +5,6 @@ import academia.devdojo.maratonajava.javacore.Dconstrutores.dominio.Anime;
 public class AnimeTeste01 {
     public static void main(String[] args) {
         Anime anime = new Anime("Akudama Drive", "TV", 12, "Ação");
-        Anime anime2 = new Anime();
         anime.imprime();
-        anime2.imprime();
-
     }
 }
