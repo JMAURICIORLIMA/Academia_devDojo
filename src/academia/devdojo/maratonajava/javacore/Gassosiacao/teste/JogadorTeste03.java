39bb9484a2229cac9d2d35d28e70c2c61ae3560b
José Maurício <mauricio.rodriguesdelima@gmail.com>
2022-05-22 12:09:25 -0300

    Aula 67 - Orientação Objetos - Associação.

---
+++ src/academia/devdojo/maratonajava/javacore/Gassosiacao/teste/JogadorTeste03.java
@@ -0,0 +1,27 @@
+package academia.devdojo.maratonajava.javacore.Gassosiacao.teste;
+
+import academia.devdojo.maratonajava.javacore.Gassosiacao.dominio.Jogador;
+import academia.devdojo.maratonajava.javacore.Gassosiacao.dominio.Time;
+
+public class JogadorTeste03 {
+    public static void main(String[] args) {
+        Jogador jogador = new Jogador("Cafu");
+        Jogador jogador2 = new Jogador("Pelé");
+
+        Time time = new Time("Brasil");
+        Jogador[] jogadores = {jogador, jogador2};
+
+        jogador.setTime(time);
+        jogador2.setTime(time);
+
+        time.setJogadores(jogadores);
+
+        System.out.println("--- Jogadores ---");
+
+        jogador.imprime();
+
+        System.out.println("--- Time ---");
+
+        time.imprime();
+    }
+}
