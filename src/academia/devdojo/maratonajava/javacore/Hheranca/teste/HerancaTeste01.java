716a37da01d17b2b723bf9a18c39458961e882a1
José Maurício <mauricio.rodriguesdelima@gmail.com>
2022-05-28 15:50:50 -0300

    Aula 71 - Orientação Objetos - Herança parte 1.

---
+++ src/academia/devdojo/maratonajava/javacore/Hheranca/teste/HerancaTeste01.java
@@ -0,0 +1,28 @@
+package academia.devdojo.maratonajava.javacore.Hheranca.teste;
+
+import academia.devdojo.maratonajava.javacore.Hheranca.dominio.Endereco;
+import academia.devdojo.maratonajava.javacore.Hheranca.dominio.Funcionario;
+import academia.devdojo.maratonajava.javacore.Hheranca.dominio.Pessoa;
+
+public class HerancaTeste01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 3");
+        endereco.setCep("12315-099");
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Toyohisa Shimazu");
+        pessoa.setCpf("123123123-00");
+        pessoa.setEndereco(endereco);
+        pessoa.imprime();
+
+        System.out.println("***************************");
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("José Augusto");
+        funcionario.setCpf("456456456-88");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(2500);
+        funcionario.imprime();
+
+    }
+}
