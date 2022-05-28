c525b3eba58a765b0aa4bd890d7bac4888cc52e7
José Maurício <mauricio.rodriguesdelima@gmail.com>
2022-05-28 16:27:01 -0300

    Aula 73 - Orientação Objetos - Herança parte 3 - visibilidade protected.

--- src/academia/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
+++ src/academia/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
@@ -7,6 +7,7 @@ public class Funcionario extends Pessoa {
     public void imprime() {
         super.imprime();
         System.out.println("Salário: " + this.salario);
+        System.out.println("Eu " + this.nome + ", recebí o salário no valor de R$ " + this.salario);
     }
 
     public double getSalario() {
