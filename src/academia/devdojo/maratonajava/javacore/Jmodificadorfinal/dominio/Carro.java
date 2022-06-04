6998a7dd119de48afc230b9b859feadcd48e60af
José Maurício <mauricio.rodriguesdelima@gmail.com>
2022-06-04 17:00:45 -0300

    Aula 78 - Orientação Objetos - Modificador final parte 02 - Tipo referência.

--- src/academia/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Carro.java
+++ src/academia/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Carro.java
@@ -2,6 +2,7 @@ package academia.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio;
 
 public class Carro {
     private String nome;
+    public final Comprador COMPRADOR = new Comprador();
     /*
         Toda constante em Java deve está com sua descrição
         em CAIXA ALTA e sepadado por _ para mais de uma palavra.
--- src/academia/devdojo/maratonajava/javacore/Jmodificadorfinal/teste/CarroTeste01.java
+++ src/academia/devdojo/maratonajava/javacore/Jmodificadorfinal/teste/CarroTeste01.java
@@ -5,6 +5,10 @@ import academia.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Carro;
 public class CarroTeste01 {
     public static void main(String[] args) {
         Carro carro = new Carro();
+
         System.out.println(Carro.VELOCIDADE_LIMITE);
+        System.out.println(carro.COMPRADOR);
+        carro.COMPRADOR.setNome("Kuririn");
+        System.out.println(carro.COMPRADOR);
     }
 }
