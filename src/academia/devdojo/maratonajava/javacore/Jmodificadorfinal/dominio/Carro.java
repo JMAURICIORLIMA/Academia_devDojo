0ed9a814c703854f48bcfc381a52128ad6aabff3
José Maurício <mauricio.rodriguesdelima@gmail.com>
2022-06-04 16:47:25 -0300

    Aula 77 - Orientação Objetos - Modificador final parte 01 - Tipo primitivo.

---
+++ src/academia/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Carro.java
@@ -0,0 +1,18 @@
+package academia.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio;
+
+public class Carro {
+    private String nome;
+    /*
+        Toda constante em Java deve está com sua descrição
+        em CAIXA ALTA e sepadado por _ para mais de uma palavra.
+     */
+    public static final double VELOCIDADE_LIMITE = 250;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
